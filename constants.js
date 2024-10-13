17a4892b7993e132fede08f13757783f9a07859f
evinReynaud <44316037+evinReynaud@users.noreply.github.com>
2024-10-13 15:22:38 +0200

    Fix autocorrect and typo

--- constants.js
+++ constants.js
@@ -10,4 +10,4 @@ const syntaxText = `Black Lotus (single card)
 4 Counterspell (creates 4 cards)
 Shatterskull Smashing (creates a card for each face)
 Jace, Vryn's Prodigy -checklist (creates a single 'checklist' style card enabled by the -checklist or -cl flag)
-leb/233 -code (finds card #233 in Limeted Edition Beta, enabled by the -code or -cd flag)`;
+leb/233 -code (finds card #233 in Limited Edition Beta, enabled by the -code or -cd flag)`;
