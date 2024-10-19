1c8ec4c770ce7beb4059746d4a9567a1bc1fc270
evinReynaud <44316037+evinReynaud@users.noreply.github.com>
2024-10-19 11:02:07 +0200

    Use magus-library getCardsAndDo for card fetching, fixing request delay issues

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Forked from [philo-jh/MTG-Proxy-Generator](https://github.com/philo-jh/MTG-Proxy
 ## What's different in this fork?
  - The cards inputted as code (with -cd) are not altered when retrieving data from scryfall
  - A dedicated button has been added to print the proxies
+ - The requests to Scryfall's API are spread out to prevent a possible ban of users IP.
 ## How to Use:
 Ever wondered how your deck would play if you just had those couple of cards? Now you can try them out before you buy with MTG Proxy-Generator! Create proxy versions of cards or even decks to playtest before purchasing. Just enter a list in MTGO format with one card name per line. You can then print them out 9 on a page and insert them over other cards in sleeves to playtest.
 ## Import Decks Using Easy MTGO Syntax
--- index.js
+++ index.js
@@ -57,68 +57,70 @@ function renderApplication(state) {
 
       STATE.deckList = [];
 
-      for(let i=0; i < queryList.length; i++) {
-        //query ScryFall for CURRENT card
-        setTimeout(() => getCard(queryList[i]).then(function (data) {
-          const card = {}
+      const numberedQueryList = queryList.map((query, index) => {
+        query.displayOrder = index;
+        return query;
+      });
 
-          card.name = data.name;
-          card.set = data.set_name;
-          card.displayOrder = i;
-          card.alternateImages = null;
-          card.editMode = false;
-          card.printsUri = data.prints_search_uri;
-          card.layout = queryList[i].layout
+      getCardsAndDo(numberedQueryList, (query, data) => {
+        const card = {}
 
-          //update card images:
-          if (data.layout == 'transform' || data.layout == 'modal_dfc') {
-            card.cardImage = (data.card_faces[0].image_uris) ? data.card_faces[0].image_uris.border_crop : "";
-            card.cardImage2 = (data.card_faces[1].image_uris) ? data.card_faces[1].image_uris.border_crop : "";
-          } else {
-            if(card.layout === 'checklist') {
-              card.layout = 'normal';
-              $(".js-results").prepend(`<div class="alert alert-danger alert-dismissible fade show col-12" role="alert">
+        card.name = data.name;
+        card.set = data.set_name;
+        card.displayOrder = query.displayOrder;
+        card.alternateImages = null;
+        card.editMode = false;
+        card.printsUri = data.prints_search_uri;
+        card.layout = query.layout
+
+        //update card images:
+        if (data.layout == 'transform' || data.layout == 'modal_dfc') {
+          card.cardImage = (data.card_faces[0].image_uris) ? data.card_faces[0].image_uris.border_crop : "";
+          card.cardImage2 = (data.card_faces[1].image_uris) ? data.card_faces[1].image_uris.border_crop : "";
+        } else {
+          if(card.layout === 'checklist') {
+            card.layout = 'normal';
+            $(".js-results").prepend(`<div class="alert alert-danger alert-dismissible fade show col-12" role="alert">
               "${card.name}" cannot be made into a checklist card. Generating standard card instead.
               <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>`);
-            }
-            card.cardImage = (data.image_uris) ? data.image_uris.border_crop : "";
           }
+          card.cardImage = (data.image_uris) ? data.image_uris.border_crop : "";
+        }
 
-          completedRequests++;
-          let percentageComplete = (completedRequests / totalRequests) * 100;
+        completedRequests++;
+        let percentageComplete = (completedRequests / totalRequests) * 100;
 
-          $(".progress-bar").css("width", `${percentageComplete}%`).attr("aria-valuenow", `${percentageComplete}`);
+        $(".progress-bar").css("width", `${percentageComplete}%`).attr("aria-valuenow", `${percentageComplete}`);
 
-          card.needsRerender = true;
+        card.needsRerender = true;
 
-          if (card.cardImage !== "") {
-            //push the cards into the deckList:
-            for (let j = 0; j < queryList[i].quantity; j++) {
-              const myTempCard = $.extend(true, {}, card);
-              STATE.deckList.push(myTempCard);
-            }
-          } else {
-            $(".js-results").prepend(`<div class="alert alert-danger alert-dismissible fade d-print-none show col-12" role="alert">
+        if (card.cardImage !== "") {
+          //push the cards into the deckList:
+          for (let j = 0; j < query.quantity; j++) {
+            const myTempCard = $.extend(true, {}, card);
+            STATE.deckList.push(myTempCard);
+          }
+        } else {
+          $(".js-results").prepend(`<div class="alert alert-danger alert-dismissible fade d-print-none show col-12" role="alert">
               "${card.name}" could not be found. Try editing your list.
               <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>`);
-          }
+        }
 
-          if (completedRequests === queryList.length) {
-            STATE.deckList = STATE.deckList.sort(function (card1, card2) {
-              return card1.displayOrder - card2.displayOrder;
-            });
+        if (completedRequests === queryList.length) {
+          STATE.deckList = STATE.deckList.sort(function (card1, card2) {
+            return card1.displayOrder - card2.displayOrder;
+          });
 
-            renderApplication(STATE);
-          }
-        }), 50);
-      }
-        
+          renderApplication(STATE);
+        }
+      });
+
       STATE.mode = MODES.REVIEW;
       editReviewPrintButtons();
     });
